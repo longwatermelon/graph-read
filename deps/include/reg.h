654ce0ec6348af9fffc2484f169663ac22f29c0c
longwatermelon <longwatermelon1@gmail.com>
2022-12-30 15:11:41 -0700

    first commit

---
+++ deps/include/reg.h
@@ -0,0 +1,135 @@
+#pragma once
+#include <cstdio>
+#include <vector>
+#include <array>
+#include <functional>
+#include <glm/glm.hpp>
+
+namespace reg
+{
+    // N: num features
+    template <size_t N>
+    struct DataPoint
+    {
+        DataPoint()
+        {
+            features.fill(0.f);
+        }
+
+        DataPoint(const std::array<float, N> &features, float y)
+            : features(features), y(y) {}
+
+        std::array<float, N> features;
+        float y{ 0.f };
+    };
+
+    namespace general
+    {
+        float calc_mean(const std::vector<float> &values);
+        float calc_sd(const std::vector<float> &values);
+        void zscore_normalize(std::vector<float> &features, float &sd, float &mean);
+
+        template <size_t N>
+        float dot(const std::array<float, N> &a, const std::array<float, N> &b)
+        {
+            float sum = 0.f;
+            for (size_t i = 0; i < a.size(); ++i)
+                sum += a[i] * b[i];
+            return sum;
+        }
+
+        // N: num features
+        template <size_t N>
+        void feature_scale(std::vector<DataPoint<N>> &data, std::array<float, N> &sd, std::array<float, N> &mean)
+        {
+            for (size_t c = 0; c < N; ++c)
+            {
+                std::vector<float> features;
+                features.reserve(data.size());
+
+                for (size_t r = 0; r < data.size(); ++r)
+                    features.emplace_back(data[r].features[c]);
+
+                zscore_normalize(features, sd[c], mean[c]);
+
+                for (size_t r = 0; r < data.size(); ++r)
+                    data[r].features[c] = features[r];
+            }
+        }
+
+        // N: num features
+        template <size_t N>
+        float cost(const std::vector<DataPoint<N>> &data,
+                   const std::function<float(const DataPoint<N>&)> &err_f)
+        {
+            float cost = 0.f;
+            for (const auto &p : data)
+                cost += err_f(p);
+            return cost / (2.f * data.size());
+        }
+
+        // N: num features
+        // func parameters: vw, vx, b
+        template <size_t N>
+        void descend(std::array<float, N> &vw, float &b, float a,
+                     const std::vector<DataPoint<N>> &data,
+                     const std::function<float(const std::array<float, N>&,
+                                               const std::array<float, N>&,
+                                               float)> &func)
+        {
+            // Calculate new b
+            float db_j = 0.f;
+            for (size_t i = 0; i < data.size(); ++i)
+                db_j += func(vw, data[i].features, b) - data[i].y;
+            db_j /= data.size();
+            float b_new = b - a * db_j;
+
+            // Calculate new vw
+            std::array<float, N> vw_new;
+            for (size_t j = 0; j < N; ++j)
+            {
+                float dw_j = 0.f;
+                for (size_t i = 0; i < data.size(); ++i)
+                    dw_j += (func(vw, data[i].features, b) - data[i].y) * data[i].features[j];
+                dw_j /= data.size();
+                /* printf("%zu: %f\n", j, dw_j); */
+
+                vw_new[j] = vw[j] - a * dw_j;
+            }
+
+            b = b_new;
+            vw = vw_new;
+        }
+    }
+
+    namespace linear
+    {
+        float f_wb(const std::array<float, 1> &w, const std::array<float, 1> &features, float b);
+    }
+
+    namespace multilinear
+    {
+        template <size_t N>
+        float f_wb(const std::array<float, N> &vw, const std::array<float, N> &features,
+                   float b)
+        {
+            // w \dot x + b
+            float res = b;
+            for (size_t i = 0; i < N; ++i)
+                res += vw[i] * features[i];
+            return res;
+        }
+    }
+
+    namespace logistic
+    {
+        float f_wb(const std::array<float, 1> &w, const std::array<float, 1> &features, float b);
+        float loss(float prediction, float data_y);
+    }
+
+    namespace multilogistic
+    {
+        float f_wb(const std::array<float, 2> &w, const std::array<float, 2> &features, float b);
+    }
+}
+
---
+++ src/main.cpp
@@ -0,0 +1,106 @@
+#include <reg.h>
+#include <graph2.h>
+
+extern "C" {
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+}
+
+#define BLACK_THRESHOLD 50
+#define IS_BLACK(pix) (pix[0] <= BLACK_THRESHOLD && pix[1] <= BLACK_THRESHOLD && pix[2] <= BLACK_THRESHOLD)
+
+bool load_image(std::vector<std::array<unsigned char, 3>> &image, const std::string &path, int &x, int &y)
+{
+    int n;
+    unsigned char* data = stbi_load(path.c_str(), &x, &y, &n, 0);
+    if (data != nullptr)
+    {
+        for (int i = 0; i < x * y * n; i += n)
+        {
+            std::array<unsigned char, 3> tmp;
+            for (int j = 0; j < 3; ++j)
+                tmp[j] = data[i + j];
+
+            image.emplace_back(tmp);
+        }
+    }
+    stbi_image_free(data);
+    return (data != nullptr);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        fprintf(stderr, "Error: no file provided\n");
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *win = SDL_CreateWindow("Graph",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            600, 600, SDL_WINDOW_SHOWN);
+    SDL_Renderer *rend = SDL_CreateRenderer(win, -1,
+            SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    // Load image
+    int w, h;
+    std::vector<std::array<unsigned char, 3>> imgdata;
+    load_image(imgdata, argv[1], w, h);
+
+    // Setup graph & data points for regression
+    graph::Graph2 graph;
+    graph.add_shape(graph::Graph2Shape(
+        {
+            { 0.f, 0.f }, { 1.f, 1.f },
+            { 1.f, 0.f }, { 0.f, 1.f }
+        },
+        { 1.f, 0.f, 0.f }
+    ));
+    graph.set_shape_size(2.f);
+    std::string graph_config = "min 0 0\nmax 1 1\nstep .2 .2\n";
+
+    std::vector<reg::DataPoint<1>> data;
+    for (size_t i = 0; i < imgdata.size(); ++i)
+    {
+        if (IS_BLACK(imgdata[i]))
+        {
+            float x = (float)(i % w) / (w - 1);
+            float y = 1.f - (float)(i - (i % w)) / w / (h - 1);
+            data.emplace_back(reg::DataPoint<1>({ x }, y));
+            graph_config += "data " + std::to_string(x) +
+                            " " + std::to_string(y) + "0\n";
+        }
+    }
+
+    graph.load(graph_config);
+
+    bool running = true;
+    SDL_Event evt;
+
+    while (running)
+    {
+        while (SDL_PollEvent(&evt))
+        {
+            switch (evt.type)
+            {
+            case SDL_QUIT:
+                running = false;
+                break;
+            }
+        }
+
+        SDL_RenderClear(rend);
+
+        graph.render(rend, { 0, 0, 600, 600 }, [](float x){ return 0.f; });
+
+        SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
+        SDL_RenderPresent(rend);
+    }
+
+    SDL_DestroyRenderer(rend);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+    return 0;
+}
+
